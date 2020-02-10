dcc8ff98eb4f5d41bf0870e52acee409d54566ea
Danichu15 <48332083+Danichu15@users.noreply.github.com>
2020-02-10 12:44:06 +0100

    Actualizado el pom para que pueda ejecutarse la página

--- practica/src/main/java/com/practica/demo/Controlador.java
+++ practica/src/main/java/com/practica/demo/Controlador.java
@@ -1,8 +1,13 @@
 package com.practica.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Controlador {
-
+	@RequestMapping("/")
+	public String index(Model model) {
+		return "index";
+	}
 }
