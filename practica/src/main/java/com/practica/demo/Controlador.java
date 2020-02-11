a60919a598f67afddfc2e66370fed3fc23bf41ba
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2020-02-11 10:22:23 +0100

    Ya funciona

--- practica/src/main/java/com/practica/demo/Controlador.java
+++ practica/src/main/java/com/practica/demo/Controlador.java
@@ -1,20 +1,22 @@
 package com.practica.demo;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@EnableAutoConfiguration
 @Controller
 public class Controlador {
 	@RequestMapping("/")
 	public String index(Model model) {
-		return "index.html"; //es necesario poner el .html
+		return "index"; //es necesario poner el .html
 	}
 	
 	@GetMapping("/load")
 	public String load(Model model) {
-		return "singIn.html"; //ver como lazar la app usando el template
+		return "singIn"; //ver como lazar la app usando el template
 	}
 }
