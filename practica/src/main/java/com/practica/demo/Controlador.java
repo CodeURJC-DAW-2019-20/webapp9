0d9ddb85acef1912767c563588ef94234451633f
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2020-02-11 09:03:20 +0100

    boton sumit

--- practica/src/main/java/com/practica/demo/Controlador.java
+++ practica/src/main/java/com/practica/demo/Controlador.java
@@ -2,6 +2,8 @@ package com.practica.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +11,10 @@ public class Controlador {
 	@RequestMapping("/")
 	public String index(Model model) {
 		return "index.html"; //es necesario poner el .html
+	}
+	
+	@GetMapping("/load")
+	public String load(Model model) {
+		return "singIn.html"; //ver como lazar la app usando el template
 	}
 }
