7def06231a2c047ceac52e934fcab7720992077d
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2020-02-17 21:45:35 +0100

    Perdon he subio mal

--- webapp9-server-brach-con-mysql-BBDD/lol/script.js
+++
@@ -1,7 +0,0 @@
-debugger
-var texto = "Only for the best League of Legends players. No errors allowed. A single failure and you will be sent to the abyss of ELO HELL"
-$(document).ready(function () {
-
-        $('#champion').append(texto)
-    
-})
