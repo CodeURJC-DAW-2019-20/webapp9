a452ac435e65ae195f9c22e356841cc01024a792
Avadem <alex.gavi@hotmail.com>
2020-02-23 18:39:19 +0100

    Update EloCalculator.java

--- practica/src/main/java/com/practica/demo/EloCalculator.java
+++ practica/src/main/java/com/practica/demo/EloCalculator.java
@@ -1,28 +1,45 @@
 package com.practica.demo;
 
 import java.lang.Math;
+import java.util.Optional;
+
+import com.practica.demo.data.Team;
 
 public class EloCalculator {
 
-private void updateElo(int teamID1,int teamID2,int result) {
-	int k1,k2;
-	//Get elo of each team from database
-	int team1Elo;
-	int team2Elo;
-	k1 = kValue(team1Elo);
-	k2 = kValue(team2Elo);
+	private TeamRepository teamRepository;
+
+	public EloCalculator(TeamRepository teamRepository) {
+		this.teamRepository = teamRepository;
+	}
+
+	public void updateElo(int teamID1, int teamID2, int result) {
+		int k1, k2;
+		// Get elo of each team from database
+		Team team1 =  teamRepository.getOne(teamID1);
+		Team team2 = teamRepository.getOne(teamID2);
 		
-	double e1;
-	double e2;
+		int team1Elo = team1.getElo();
+		int team2Elo = team2.getElo();
+		k1 = kValue(team1Elo);
+		k2 = kValue(team2Elo);
+
+		double e1;
+		double e2;
+
+		e1 = eCalc(team1Elo, team2Elo);
+		e2 = eCalc(team2Elo, team1Elo);
+
+		team1Elo = newElo(team1Elo, e1, k1, result);
+		team2Elo = newElo(team2Elo, e1, k1, 1 - result);
 		
-	e1 = eCalc(team1Elo,team2Elo);
-	e2 = eCalc(team2Elo,team1Elo);	
-	
-	team1Elo = newElo(team1Elo,e1,k1,result);
-	team2Elo = newElo(team2Elo,e1,k1,1-result);	
-	
-	//Update elos in database
-	
+		team1.setElo(team1Elo);
+		team2.setElo(team2Elo);
+		
+		teamRepository.save(team1);
+		teamRepository.save(team2);
+		// Update elos in database
+
 	}
 
 	private int kValue(int teamsElo) {
@@ -39,16 +56,16 @@ private void updateElo(int teamID1,int teamID2,int result) {
 
 	private double eCalc(int ownElo, int enemyElo) {
 		double value;
-		double aux; 
+		double aux;
 		aux = ((enemyElo - ownElo) / 400);
-		aux = Math.pow(10,aux);
-		value =  1 / aux;
+		aux = Math.pow(10, aux);
+		value = 1 / aux;
 		return value;
 	}
-	
+
 	private int newElo(int oldElo, double estimatedScore, int kValue, int result) {
 		int value;
-		value = (int) ( oldElo + (kValue * (result - estimatedScore) ));
+		value = (int) (oldElo + (kValue * (result - estimatedScore)));
 		return value;
 	}
 }
