b322e83bd09ba6bd1aafa02813dfaa33449394d8
luuromero97 <luuromero97@users.noreply.github.com>
2020-02-23 20:44:37 +0100

    datos

--- Dump20200220 (1)/web_fase_1_teams_on_game.sql
+++ Dump20200220 (1)/web_fase_1_teams_on_game.sql
@@ -27,13 +27,13 @@ CREATE TABLE `teams_on_game` (
   `game_id_game` int NOT NULL,
   `result` int NOT NULL,
   `winner` tinyint(1) DEFAULT NULL,
-  `date` varchar(45) NOT NULL,
+  `date` varchar(45) COLLATE utf8mb4_da_0900_ai_ci NOT NULL,
   PRIMARY KEY (`team_id_team`,`game_id_game`),
   KEY `fk_team_has_game_game1_idx` (`game_id_game`),
   KEY `fk_team_has_game_team1_idx` (`team_id_team`),
   CONSTRAINT `fk_team_has_game_game1` FOREIGN KEY (`game_id_game`) REFERENCES `game` (`id_game`),
   CONSTRAINT `fk_team_has_game_team1` FOREIGN KEY (`team_id_team`) REFERENCES `team` (`id_team`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_da_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -55,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-22 20:43:19
+-- Dump completed on 2020-02-23 20:43:23
