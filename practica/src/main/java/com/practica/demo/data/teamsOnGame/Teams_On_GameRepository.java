e8ad4c9fc99fca2ffcf0987c767d3bf361418980
Avadem <alex.gavi@hotmail.com>
2020-02-29 13:34:45 +0100

    Merge y update de bd

--- practica/src/main/java/com/practica/demo/data/teamsOnGame/Teams_On_GameRepository.java
+++ practica/src/main/java/com/practica/demo/data/teamsOnGame/Teams_On_GameRepository.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import com.practica.demo.data.Teams_On_Game;
+
 
 
 
