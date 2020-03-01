0acb9d1f1a310333d55005b27c9067f9ec0d11a5
luuromero97 <luuromero97@users.noreply.github.com>
2020-03-01 18:47:18 +0100

    comprobacion unirse torneo

--- practica/src/main/java/com/practica/demo/GameRepository.java
+++ practica/src/main/java/com/practica/demo/GameRepository.java
@@ -1,9 +1,13 @@
 package com.practica.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.practica.demo.data.Game;
 
 public interface GameRepository extends JpaRepository<Game, Integer> {
+
+	List<Game> findBytournamentIdTournament(int idTournament);
 
 }
