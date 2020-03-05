ef57bc97488194b2bbbc53421eb9d573cc7e49c7
Avadem <alex.gavi@hotmail.com>
2020-03-05 20:18:21 +0100

    Nontiendo

--- practica/src/main/java/com/practica/demo/data/player/PlayerRestController.java
+++ practica/src/main/java/com/practica/demo/data/player/PlayerRestController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.practica.demo.data.user.User;
 
 @RestController
 public class PlayerRestController {
@@ -26,6 +29,14 @@ public class PlayerRestController {
 			return new ResponseEntity<>(aux.get(), HttpStatus.OK);
 		}else {
 			return new  ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	@RequestMapping(value = "/api/player", method = RequestMethod.PUT)
+	public ResponseEntity<Object> createPlayer(@RequestBody User user){
+		if (playerService.createPlayer(user)){
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		}else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 	}
 }
