6ace090fb4c3ba0c2a91e346f91891cea4cb4767
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2020-03-10 00:27:14 +0100

    .jar y dockfile

---
+++ practica/target - copia/classes/static/js/Leaderboard/load.js
@@ -0,0 +1,43 @@
+function showTeam(pos,team) {
+	if(team.id_team != 0){
+		console.log(team.id_team);
+$("#table").append(
+				
+				'<tr>' +
+				'<th scope="row">' + pos + '</th>' +
+				'<td>' + team.name + '</td>' +
+				'<td>' + team.elo + '</td>' +
+				'</tr>'
+			);
+		
+	}	
+
+}
+
+function loadTeams(callback) {
+    $.ajax({
+        url: 'https://localhost:8443/api/leaderBoardLoaded?_csrf='+csrf
+    }).done(function (teams) {
+    	$('#loader').empty();
+    	callback(teams);
+    });
+}
+
+
+$(document).ready(function(){
+	var cont = 2;
+$("#btn").on('click', function() {
+	$('#loader').html("<img src='/imgs/spinner.gif'/>");
+		loadTeams(function (teams) {
+	        for (var i = cont; i < teams.length; i++) {
+	        	cont = i + 1;
+	        	//console.log(cont);
+	            showTeam(cont,teams[i]);	
+	        }
+	    });
+	});
+	
+});
+	
+
+	
---
+++ practica/target - copia/classes/static/js/TournamentPage/googleMap.js
@@ -0,0 +1,38 @@
+function loadCoordenates(callback) {
+    $.ajax({
+        url: 'https://localhost:8443/loadCoordenates?name='+name
+    }).done(function (coordenates) {
+    	console.log("olo " + coordenates.latitude);
+    	callback(coordenates);
+    });
+}
+
+function loadMap(coordenates) {
+    var latlng = new google.maps.LatLng(coordenates.latitude, coordenates.longitude);
+    
+    var myOptions = {
+    	      zoom: 4,
+    	      center: latlng,
+    	      mapTypeId: google.maps.MapTypeId.ROADMAP
+    	    };
+    
+    var map = new google.maps.Map(document.getElementById("map_container"),myOptions);
+    
+    var marker = new google.maps.Marker({
+        position: latlng, 
+        map: map, 
+        title:"Tournament Title"
+      });
+    
+    
+  }
+
+$(document).ready(function(){
+	loadCoordenates(function(coordenates){
+		loadMap(coordenates);
+	});
+});
+
+
+
+
