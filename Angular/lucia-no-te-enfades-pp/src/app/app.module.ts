bad435daba5a3abb72f41efbcb1df07608f1199f
Daniel <danielliszewski@hotmail.com>
2020-03-27 16:00:19 +0100

    Añadido contendi team service

--- Angular/lucia-no-te-enfades-pp/src/app/app.module.ts
+++ Angular/lucia-no-te-enfades-pp/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
+    HttpClientModule,
     NgbModule
   ],
   providers: [],
---
+++ Angular/lucia-no-te-enfades-pp/src/app/teams.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Team } from './team.model';
+
+const BASE_URL = 'http://127.0.0.1:8443/teams/';
+
+@Injectable({ providedIn: 'root' })
+export class TeamsService {
+    
+    page: number;
+
+    constructor(private httpClient: HttpClient){}
+
+    //TODO get LeaderBoard
+
+    getTeams(): Observable<Team[]> {
+        return this.httpClient.get(BASE_URL + "page=" + this.page).pipe(
+            catchError(error => this.handleError(error))
+        ) as Observable<Team[]>;
+    }
+
+    addTeam(team: Team) {
+        return this.httpClient.post(BASE_URL, team).pipe(
+            catchError(error => this.handleError(error))
+        );
+    }
+
+    updateTeam(team: Team) {
+        return this.httpClient.put(BASE_URL + team.idTeam, team).pipe(
+            catchError(error => this.handleError(error))
+        );
+    }
+
+    private handleError(error: any) {
+		console.error(error);
+		return Observable.throw("Server error (" + error.status + "): " + error.text())
+	}
+
+}
