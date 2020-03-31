9b9b39c1cecbb84f4e7b80a6244b3548c682af03
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2020-03-31 19:33:25 +0200

    ejecuta pero las rutas hacen lo que quieren

--- Angular/lucia-no-te-enfades-pp/src/app/app.module.ts
+++ Angular/lucia-no-te-enfades-pp/src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { LoginComponent } from './login/login.component';
 import { UserService } from './_servicies/user.service';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NgbModule,
-    LoginComponent
+    RouterModule.forRoot([
+      {path: 'login', component:LoginComponent},
+      {path:'index', component:AppComponent}
+     // {path:"",redirectTo:"index",pathMatch:'full'}
+    ])  
   ],
   providers: [
     UserService
