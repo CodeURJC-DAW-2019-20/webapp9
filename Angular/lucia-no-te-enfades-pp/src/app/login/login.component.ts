5a4eb674c5cd9a2202799c6ea2e55f85928eaed5
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2020-04-07 19:16:39 +0200

    cambios

--- Angular/lucia-no-te-enfades-pp/src/app/login/login.component.ts
+++ Angular/lucia-no-te-enfades-pp/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { first } from 'rxjs/operators';
+import {UserService} from '../_servicies/user.service'
 
 //import { AlertService, AuthenticationService } from '@/_services';
 
@@ -9,12 +10,52 @@ import { first } from 'rxjs/operators';
     selector: 'login',
     templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
-    ngOnInit() {
    
+    credentials = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    });
+    message = ('');
+    wrongInput: string = ('qwrong Input');
+    emptyPassword: string;
+    emptyUsername: string;
+    error: boolean;
+    returnUrl:string;
+     
+    constructor(public userService: UserService, public router: Router, public route:ActivatedRoute) {
+        
+      }
 
-        // get return url from route parameters or default to '/'
-       // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+      login(username: string, password: string, event: Event) {
+
+        if(this.credentials.controls['email'].value == ('')){
+            return
+        }
+        if (this.credentials.controls['password'].value == ('')) {
+            return
+        }
+        this.userService.login(username, password).subscribe(
+            res => {
+              console.log(res);
+              this.navigate();
+            },
+            error => {
+              this.error = true;
+              this.message = "Wrong data.";
+              console.error(error);
+            }
+          );
+    
+        }
+   
+    ngOnInit() {
+        this.error = false; 
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/TenniShip';
+   
     }
+    navigate() {
+        this.router.navigate([this.returnUrl]);
+      }
 
     // convenience getter for easy access to form fields
 
