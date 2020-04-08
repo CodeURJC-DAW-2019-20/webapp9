1bed65498d8ad90d98b53cc971c8df618d6c5248
Daniel <danielliszewski@hotmail.com>
2020-04-08 17:13:13 +0200

    aspecto de la pagina web parcialmente restaurado, routers funcionales

--- Angular/lucia-no-te-enfades-pp/src/app/home/home.component.ts
+++ Angular/lucia-no-te-enfades-pp/src/app/home/home.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  imgCarr1Url = "assets/main1.jpg";
+  imgCarr2Url = "assets/main2.jpg";
+  imgCarr3Url = "assets/main3.jpg";
+  imgRlIcon = "assets/rl-icon.jpg";
+  imgLeaderboard = "assets/leaderboardLogo.png";
+  imgUserLogo = "assets/userLogo.png";
+  imgWebIconUrl = "assets/web-icon-white.png";
+  
   constructor() { }
 
   ngOnInit(): void {
