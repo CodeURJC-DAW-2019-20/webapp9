13be5898f8085ac81f55e057163d268c53d5f505
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2020-04-19 14:25:52 +0200

    Para docker en sping paso1

--- practica/src/main/java/com/practica/demo/security/RestSecurityConfig.java
+++ practica/src/main/java/com/practica/demo/security/RestSecurityConfig.java
@@ -18,7 +18,7 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+		http.authorizeRequests().antMatchers("/new/**").permitAll();
 		http.antMatcher("/api/**");
 		
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/logIn").authenticated();
